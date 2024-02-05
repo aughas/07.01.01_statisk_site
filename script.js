fc33f4f97a169ad3ae662477d734389636710157
August <auha0001@stud.kea.dk>
2024-02-05 16:23:30 +0100

    massa script osv, urmarket är det originala och template i botten e nytt

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 window.addEventListener("DOMContentLoaded", init);
 
-const clothURL = "https://kea-alt-del.dk/t7/api/products?limit=50&start=10";
+const clothesURL = "https://kea-alt-del.dk/t7/api/products";
 
 let clothesTemplate;
 let clothesContainer;
+
+//hämta kod från json url^^^^ (?)
+function init() {
+  console.log("init");
+
+  clothesTemplate = document.querySelector(".clothes_template");
+  console.log("clothesTemplate", clothesTemplate);
+
+  clothesContainer = document.querySelector(".clothes_container");
+  console.log("clothes_container", clothesContainer);
+
+  fetch(clothesURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      showclothes(json);
+    });
+}
+
+function showclothes(clothesJSON) {
+  let clothesClone;
+
+  clothesJSON.forEach((clothes) => {
+    console.log("clothes", clothes);
+    clothesClone = clothesTemplate.cloneNode(true).content;
+    clothesClone.querySelector(".clothes_image").src = `https://kea-alt-del.dk/t7/images/webp/640/${clothes.id}.webp`;
+    clothesClone.querySelector(".clothes_image").alt = `Picture of a ${clothes.name} clothes`;
+    clothesClone.querySelector(".clothes_name").textContent = clothes.name;
+    clothesClone.querySelector(".clothes_tagline").textContent = clothes.tagline;
+    clothesClone.querySelector(".clothes_description").textContent = clothes.description;
+    clothesClone.querySelector("a").href = `single.html?id=${clothes.id}`;
+    clothesContainer.appendChild(clothesClone);
+  });
+}
