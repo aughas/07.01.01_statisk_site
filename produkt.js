c0b4b55ea3ad9a85c86ad4a086c417a70f5a1110
August <auha0001@stud.kea.dk>
2024-02-07 16:02:27 +0100

    rätta namn för klasser

--- produkt.js
+++ produkt.js
@@ -1,41 +1,30 @@
-window.addEventListener("DOMContentLoaded", initBeer);
+window.addEventListener("DOMContentLoaded", initClothes);
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-function initBeer() {
+function initClothes() {
   fetch(`https://kea-alt-del.dk/t7/api/products/${id}`)
     .then((response) => response.json())
     .then((json) => {
-      showSingleBeer(json);
+      showSingleClothes(json);
     });
 }
 
-function showSingleBeer(beer) {
-  console.log("beer", beer);
+function showSingleClothes(clothes) {
+  console.log("clothes", clothes);
 
-  document.querySelector(".single_beer_info img").src = `https://kea-alt-del.dk/t7/images/webp/640/${beer.id}.webp`;
+  document.querySelector(".single_clothes_info img").src = `https://kea-alt-del.dk/t7/images/webp/640/${clothes.id}.webp`;
 
-  document.querySelector("h2").textContent = beer.productdisplayname;
+  document.querySelector(".single_brand").textContent = clothes.productdisplayname;
 
-  document.querySelector(".single_beer_describtion").textContent = `${beer.articletype} / ${beer.brandname}`;
-  document.querySelector(".price").textContent = `${beer.price} DKK:-`;
+  document.querySelector(".single_clothes_describtion").textContent = `${clothes.articletype} / ${clothes.brandname}`;
+  document.querySelector(".price").textContent = `${clothes.price} DKK:-`;
 
-  document.querySelector(".list_gender").textContent = beer.gender;
-  document.querySelector(".list_season").textContent = beer.season;
-  document.querySelector(".list_articletype").textContent = beer.articletype;
+  document.querySelector(".list_gender").textContent = clothes.gender;
+  document.querySelector(".list_season").textContent = clothes.season;
+  document.querySelector(".list_articletype").textContent = clothes.articletype;
 
-  document.querySelector("h1").textContent = beer.brandname;
-  document.querySelector(".year").textContent = `Product made in ${beer.productionyear}`;
-
-  document.querySelector(".single_beer_info p").textContent = `Beer by ${beer[0].contributed_by}`;
-
-  console.log("food_pairing", beer[0].food_pairing);
-
-  let foodPairingList = document.querySelector(".single_beer_info ul");
-  beer[0].food_pairing.forEach((paring) => {
-    const item = document.createElement("li");
-    item.textContent = paring;
-    foodPairingList.appendChild(item);
-  });
+  document.querySelector(".single_brand_h1").textContent = clothes.brandname;
+  document.querySelector(".year").textContent = `Product made in ${clothes.productionyear}`;
 }
