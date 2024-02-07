1dabfea7cf873942627e73c766f0babe35a5b18c
August <auha0001@stud.kea.dk>
2024-02-07 15:38:05 +0100

    single product view API korrekt, fel namv

--- produkt.js
+++ produkt.js
@@ -14,13 +14,20 @@ function initBeer() {
 function showSingleBeer(beer) {
   console.log("beer", beer);
 
-  document.querySelector("h1").textContent = beer.productdisplayname;
-  document.querySelector("header p").textContent = beer[0].tagline;
+  document.querySelector(".single_beer_info img").src = `https://kea-alt-del.dk/t7/images/webp/640/${beer.id}.webp`;
 
-  document.querySelector(".single_beer_describtion").textContent = beer[0].description;
-  document.querySelector(".single_beer_tips").textContent = beer[0].brewers_tips;
+  document.querySelector("h2").textContent = beer.productdisplayname;
 
-  document.querySelector(".single_beer_info img").src = beer[0].image_url;
+  document.querySelector(".single_beer_describtion").textContent = `${beer.articletype} / ${beer.brandname}`;
+  document.querySelector(".price").textContent = `${beer.price} DKK:-`;
+
+  document.querySelector(".list_gender").textContent = beer.gender;
+  document.querySelector(".list_season").textContent = beer.season;
+  document.querySelector(".list_articletype").textContent = beer.articletype;
+
+  document.querySelector("h1").textContent = beer.brandname;
+  document.querySelector(".year").textContent = `Product made in ${beer.productionyear}`;
+
   document.querySelector(".single_beer_info p").textContent = `Beer by ${beer[0].contributed_by}`;
 
   console.log("food_pairing", beer[0].food_pairing);
